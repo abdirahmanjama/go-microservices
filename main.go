3d4fb0c61d938c93ff2a4eb30fc83869facd4eb3
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-08 17:40:38 +0100

    add error handling

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,6 +10,13 @@ import (
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World!")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "error", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
 	})
 
 	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
