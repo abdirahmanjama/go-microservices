2dbb27697bdf45fe751cdc65b155c24e4e217185
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-08 17:20:02 +0100

    add another handler function

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye")
+	})
 
 	http.ListenAndServe(":9090", nil)
 }
